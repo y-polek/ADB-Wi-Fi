7bb0dea68c45288323556a43af97f6987d09e049
Yury Polek <yury.polek@gmail.com>
2020-07-13 23:26:20 +0300

    make WindowFactory DumbAware to show Tool Window while indexing

--- src/main/kotlin/dev/polek/adbwifi/ui/WindowFactory.kt
+++ src/main/kotlin/dev/polek/adbwifi/ui/WindowFactory.kt
@@ -1,11 +1,12 @@
 package dev.polek.adbwifi.ui
 
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
 
-class WindowFactory : ToolWindowFactory {
+class WindowFactory : ToolWindowFactory, DumbAware {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val contentFactory = ContentFactory.SERVICE.getInstance()
