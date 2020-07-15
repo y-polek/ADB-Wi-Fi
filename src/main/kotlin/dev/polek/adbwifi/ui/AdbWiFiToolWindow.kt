0a4dd7cfef944c9cbe3ff77ced715b6bfc2a8a88
Yury Polek <yury.polek@gmail.com>
2020-07-15 23:47:52 +0300

    add Button renderer for devices table

--- src/main/kotlin/dev/polek/adbwifi/ui/AdbWiFiToolWindow.kt
+++ src/main/kotlin/dev/polek/adbwifi/ui/AdbWiFiToolWindow.kt
@@ -4,22 +4,49 @@ import com.intellij.openapi.wm.ToolWindow
 import com.intellij.ui.table.JBTable
 import dev.polek.adbwifi.MyBundle
 import dev.polek.adbwifi.adb.Adb
+import dev.polek.adbwifi.model.Device
 import java.awt.BorderLayout
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import javax.swing.JButton
 import javax.swing.JPanel
+
 
 class AdbWiFiToolWindow(private val toolWindow: ToolWindow) : JPanel() {
 
     private val tableModel = DevicesTableModel()
-    private val table = JBTable(tableModel)
+    private val table = JBTable(tableModel).apply {
+        emptyText.text = MyBundle.message("noConnectedDevices")
+        //rowSelectionAllowed = false
+        //cellSelectionEnabled = false
+        getColumn(DevicesTableModel.Columns.CONNECTED_STATE.name).cellRenderer = ButtonCellRenderer()
+        addMouseListener(object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent) {
+                val table = this@apply
+                val column = table.columnModel.getColumnIndexAtX(e.x)
+
+                val row = e.y / table.rowHeight
+
+                /*Checking the row or column is valid or not*/
+                if (row < table.rowCount && row >= 0 && column < table.columnCount && column >= 0) {
+                    val value = table.getValueAt(row, column)
+                    (value as? JButton)?.doClick()
+                }
+            }
+        })
+    }
     private val adb = Adb()
 
     init {
         layout = BorderLayout()
         add(table, BorderLayout.CENTER)
 
-        table.emptyText.text = MyBundle.message("noConnectedDevices")
 
 
-        tableModel.devices = adb.devices()
+
+        tableModel.devices = listOf(
+                Device("123", "Samsung", "192.168.1.101", false),
+                Device("12345", "OnePlus", "192.168.1.102", true)
+        )
     }
 }
--- src/main/kotlin/dev/polek/adbwifi/ui/DevicesTableModel.kt
+++ src/main/kotlin/dev/polek/adbwifi/ui/DevicesTableModel.kt
@@ -2,6 +2,8 @@ package dev.polek.adbwifi.ui
 
 import dev.polek.adbwifi.model.Device
 import java.lang.RuntimeException
+import javax.swing.JButton
+import javax.swing.JOptionPane
 import javax.swing.table.AbstractTableModel
 
 class DevicesTableModel : AbstractTableModel() {
@@ -14,7 +16,11 @@ class DevicesTableModel : AbstractTableModel() {
 
     override fun getRowCount() = devices.size
 
-    override fun getColumnCount() = 4
+    override fun getColumnCount() = Columns.values().size
+
+    override fun getColumnName(column: Int): String {
+        return Columns.values()[column].name
+    }
 
     override fun getValueAt(rowIndex: Int, columnIndex: Int): Any {
         val device = devices[rowIndex]
@@ -22,9 +28,23 @@ class DevicesTableModel : AbstractTableModel() {
             0 -> device.name
             1 -> device.id
             2 -> device.address
-            3 -> device.isConnected
+            3 -> createButton(if (device.isConnected) "Disconnect" else "Connect", rowIndex)
             else -> throw RuntimeException("Unknown column #$columnIndex")
         }
     }
 
+    private fun createButton(text: String, rowIndex: Int): JButton {
+        val button = JButton(text)
+        button.addActionListener {
+            JOptionPane.showMessageDialog(JOptionPane.getFrameForComponent(button), "Button clicked $rowIndex")
+        }
+        return button
+    }
+
+    enum class Columns {
+        NAME,
+        ID,
+        ADDRESS,
+        CONNECTED_STATE
+    }
 }
