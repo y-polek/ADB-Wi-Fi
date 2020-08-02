353e4d1304ce8f77efdff43e8c72ff913e67d51d
Yury Polek <yury.polek@gmail.com>
2020-08-02 12:50:12 +0300

    fix IP address parsing

--- src/main/kotlin/dev/polek/adbwifi/adb/Adb.kt
+++ src/main/kotlin/dev/polek/adbwifi/adb/Adb.kt
@@ -13,9 +13,9 @@ class Adb {
                     DEVICE_ID_REGEX.matchEntire(line)?.groupValues?.get(1)
                 }
                 .map { deviceId ->
-                    val model = "adb -s $deviceId shell getprop ro.product.model".exec().firstOrNull().orEmpty()
-                    val manufacturer = "adb -s $deviceId shell getprop ro.product.manufacturer".exec().firstOrNull().orEmpty()
-                    val address = "adb -s $deviceId shell ip route | awk '{print \$9}'".exec().firstOrNull().orEmpty()
+                    val model = model(deviceId)
+                    val manufacturer = manufacturer(deviceId)
+                    val address = address(deviceId)
                     Device(
                             id = deviceId,
                             name = "$manufacturer $model".trim(),
@@ -24,12 +24,28 @@ class Adb {
                 .toList()
     }
 
+    private fun model(deviceId: String): String {
+        return "adb -s $deviceId shell getprop ro.product.model".exec().firstLine().trim()
+    }
+
+    private fun manufacturer(deviceId: String): String {
+        return "adb -s $deviceId shell getprop ro.product.manufacturer".exec().firstLine().trim()
+    }
+
+    private fun address(deviceId: String): String {
+        val address = DEVICE_ADDRESS_REGEX.matchEntire("adb -s $deviceId shell ip route".exec().firstLine())?.groupValues?.get(1)
+        return address.orEmpty()
+    }
+
     companion object {
         private val DEVICE_ID_REGEX = "(.*?)\\s+device".toRegex()
+        private val DEVICE_ADDRESS_REGEX = ".*\\b(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})\\b.*".toRegex()
 
         private fun String.exec(): Sequence<String> {
             val process = Runtime.getRuntime().exec(this)
             return BufferedReader(InputStreamReader(process.inputStream)).lineSequence()
         }
+
+        private fun Sequence<String>.firstLine(): String = this.firstOrNull().orEmpty()
     }
 }
