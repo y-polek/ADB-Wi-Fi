2b75a173f4d0d296dbf42ca4b3b3bb4630779494
Yury Polek <yury.polek@gmail.com>
2020-08-04 00:24:47 +0300

    add split pane to ToolWindow

--- src/main/kotlin/dev/polek/adbwifi/services/ShellService.kt
+++ src/main/kotlin/dev/polek/adbwifi/services/ShellService.kt
@@ -5,6 +5,12 @@ import com.intellij.openapi.Disposable
 class ShellService : Disposable {
 
     var isShellVisible = false
+        set(value) {
+            field = value
+            shellVisibilityListener?.invoke(value)
+        }
+
+    var shellVisibilityListener: ((isVisible: Boolean) -> Unit)? = null
 
     override fun dispose() {
 
--- src/main/kotlin/dev/polek/adbwifi/ui/AdbWiFiToolWindow.kt
+++ src/main/kotlin/dev/polek/adbwifi/ui/AdbWiFiToolWindow.kt
@@ -5,15 +5,17 @@ import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.wm.ToolWindow
+import com.intellij.ui.JBSplitter
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.util.ui.components.BorderLayoutPanel
-import dev.polek.adbwifi.adb.Adb
-import dev.polek.adbwifi.model.Device
 import dev.polek.adbwifi.services.AdbService
+import dev.polek.adbwifi.services.ShellService
 
 class AdbWiFiToolWindow(private val toolWindow: ToolWindow) : BorderLayoutPanel() {
 
+    private val splitter = JBSplitter(true, "AdbWifi.ShellPaneProportion", 0.6f)
     private val deviceListPanel = DeviceListPanel()
+    private val shellPanel = JBScrollPane()
 
     init {
         val actionManager = ActionManager.getInstance()
@@ -22,11 +24,23 @@ class AdbWiFiToolWindow(private val toolWindow: ToolWindow) : BorderLayoutPanel(
         toolbar.setTargetComponent(this)
         addToTop(toolbar.component)
 
-        addToCenter(JBScrollPane(deviceListPanel))
+        splitter.firstComponent = JBScrollPane(deviceListPanel)
+
+        addToCenter(splitter)
+
+        val shellService = ServiceManager.getService(ShellService::class.java)
+        updateShellPanel(shellService.isShellVisible)
+        shellService.shellVisibilityListener = { isVisible ->
+            updateShellPanel(isVisible)
+        }
 
         val adbService = ServiceManager.getService(AdbService::class.java)
         adbService.deviceListListener = { devices ->
             deviceListPanel.devices = devices
         }
+    }
+
+    private fun updateShellPanel(isShellVisible: Boolean) {
+        splitter.secondComponent = if (isShellVisible) shellPanel else null
     }
 }
