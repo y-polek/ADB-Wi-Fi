901fea9c5f9d2deeb389f291f5bc2b89d413a8d9
Yury Polek <yury.polek@gmail.com>
2020-08-25 19:53:17 +0300

    use full path to ADB executable

--- src/main/kotlin/dev/polek/adbwifi/adb/Adb.kt
+++ src/main/kotlin/dev/polek/adbwifi/adb/Adb.kt
@@ -1,16 +1,22 @@
 package dev.polek.adbwifi.adb
 
+import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.logger
 import dev.polek.adbwifi.commandexecutor.CommandExecutor
 import dev.polek.adbwifi.model.Device
 import dev.polek.adbwifi.model.LogEntry
+import dev.polek.adbwifi.services.PropertiesService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.flow
+import java.io.IOException
 
 class Adb(private val commandExecutor: CommandExecutor) {
 
+    private val properties = service<PropertiesService>()
+
     fun devices(): List<Device> {
-        val devices = "adb devices".exec()
+        val devices = "devices".exec()
             .drop(1)
             .mapNotNull { line ->
                 DEVICE_ID_REGEX.matchEntire(line)?.groupValues?.get(1)?.trim()
@@ -49,36 +55,36 @@ class Adb(private val commandExecutor: CommandExecutor) {
     }
 
     fun connect(device: Device): Flow<LogEntry> = flow {
-        "adb -s ${device.id} tcpip 5555".execAndLog(this)
-        "adb connect ${device.address}:5555".execAndLog(this)
+        "-s ${device.id} tcpip 5555".execAndLog(this)
+        "connect ${device.address}:5555".execAndLog(this)
     }
 
     fun disconnect(device: Device): Flow<LogEntry> = flow {
-        "adb disconnect ${device.address}:5555".execAndLog(this)
+        "disconnect ${device.address}:5555".execAndLog(this)
     }
 
     private fun androidId(deviceId: String): String {
-        return "adb -s $deviceId shell settings get secure android_id".exec().firstLine()
+        return "-s $deviceId shell settings get secure android_id".exec().firstLine()
     }
 
     private fun model(deviceId: String): String {
-        return "adb -s $deviceId shell getprop ro.product.model".exec().firstLine().trim()
+        return "-s $deviceId shell getprop ro.product.model".exec().firstLine().trim()
     }
 
     private fun manufacturer(deviceId: String): String {
-        return "adb -s $deviceId shell getprop ro.product.manufacturer".exec().firstLine().trim()
+        return "-s $deviceId shell getprop ro.product.manufacturer".exec().firstLine().trim()
     }
 
     private fun androidVersion(deviceId: String): String {
-        return "adb -s $deviceId shell getprop ro.build.version.release".exec().firstLine().trim()
+        return "-s $deviceId shell getprop ro.build.version.release".exec().firstLine().trim()
     }
 
     private fun apiLevel(deviceId: String): String {
-        return "adb -s $deviceId shell getprop ro.build.version.sdk".exec().firstLine().trim()
+        return "-s $deviceId shell getprop ro.build.version.sdk".exec().firstLine().trim()
     }
 
     private fun address(deviceId: String): String? {
-        val firstLine = "adb -s $deviceId shell ip route".exec().firstLine()
+        val firstLine = "-s $deviceId shell ip route".exec().firstLine()
         return DEVICE_ADDRESS_REGEX.matchEntire(firstLine)?.groupValues?.get(1)
     }
 
@@ -89,7 +95,19 @@ class Adb(private val commandExecutor: CommandExecutor) {
         }
     }
 
-    private fun String.exec(): Sequence<String> = commandExecutor.exec(this)
+    private fun adb(args: String): String {
+        return "${properties.adbLocation}/adb $args"
+    }
+
+    private fun String.exec(): Sequence<String> {
+        val command = adb(this)
+        return try {
+            commandExecutor.exec(command)
+        } catch (e: IOException) {
+            LOG.error("Failed to execute command: '$command'", e)
+            emptySequence()
+        }
+    }
 
     private fun String.execSilently(): String {
         return this.exec().joinToString(separator = "\n")
@@ -102,6 +120,8 @@ class Adb(private val commandExecutor: CommandExecutor) {
     }
 
     companion object {
+        private val LOG = logger("Adb")
+
         private val DEVICE_ID_REGEX = "(.*?)\\s+device".toRegex()
         private val DEVICE_ADDRESS_REGEX = ".*\\b(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})\\b.*".toRegex()
         private val IS_IP_ADDRESS_REGEX = "(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})(:\\d{1,5})?".toRegex()
--- src/main/kotlin/dev/polek/adbwifi/model/LogEntry.kt
+++ src/main/kotlin/dev/polek/adbwifi/model/LogEntry.kt
@@ -1,6 +1,6 @@
 package dev.polek.adbwifi.model
 
 sealed class LogEntry(val text: String) {
-    class Command(text: String) : LogEntry(text)
+    class Command(args: String) : LogEntry("adb $args")
     class Output(text: String) : LogEntry(text)
 }
