d1af0103659ed900cea1a766996788c61e1e156f
Yury Polek <yury.polek@gmail.com>
2020-08-26 01:41:44 +0300

    verify 'adb' binary location

--- src/main/kotlin/dev/polek/adbwifi/settings/AdbWifiConfigurable.kt
+++ src/main/kotlin/dev/polek/adbwifi/settings/AdbWifiConfigurable.kt
@@ -6,7 +6,9 @@ import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.ui.TextComponentAccessor
 import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.util.SystemInfo
+import com.intellij.ui.JBColor
 import com.intellij.ui.components.JBLabel
 import dev.polek.adbwifi.MyBundle
 import dev.polek.adbwifi.services.PropertiesService
@@ -22,6 +24,22 @@ class AdbWifiConfigurable : Configurable {
 
     private val properties = service<PropertiesService>()
     private lateinit var textField: TextFieldWithBrowseButton
+    private lateinit var statusLabel: JBLabel
+
+    private val textComponentAccessor = object : TextComponentAccessor<JTextField> {
+        override fun getText(component: JTextField) = component.text
+
+        override fun setText(component: JTextField, text: String) {
+            val file = File(text)
+            val dirName = if (file.isFile) {
+                file.parent.orEmpty()
+            } else {
+                text
+            }
+            component.text = dirName
+            verifyAdbLocation()
+        }
+    }
 
     override fun getDisplayName(): String {
         return MyBundle.getMessage("settingsPageName")
@@ -36,6 +54,7 @@ class AdbWifiConfigurable : Configurable {
             GridBagConstraints().apply {
                 gridx = 0
                 gridy = 0
+                insets = Insets(0, 0, 0, 8)
             }
         )
 
@@ -47,7 +66,7 @@ class AdbWifiConfigurable : Configurable {
             null,
             null,
             adbLocationChooserDescriptor(),
-            DIRECTORY_TEXT_COMPONENT_ACCESSOR
+            textComponentAccessor
         )
         panel.add(
             textField,
@@ -56,9 +75,23 @@ class AdbWifiConfigurable : Configurable {
                 gridy = 0
                 fill = GridBagConstraints.HORIZONTAL
                 weightx = 1.0
-                insets = Insets(0, 8, 0, 0)
             }
         )
+
+        statusLabel = JBLabel("'adb' binary found.")
+        statusLabel.icon = IconLoader.getIcon("AllIcons.General.InspectionsError")
+        panel.add(
+            statusLabel,
+            GridBagConstraints().apply {
+                gridx = 1
+                gridy = 1
+                fill = GridBagConstraints.HORIZONTAL
+                weightx = 1.0
+                insets = Insets(4, 0, 0, 0)
+            }
+        )
+
+        verifyAdbLocation()
 
         return panel(top = panel)
     }
@@ -73,6 +106,7 @@ class AdbWifiConfigurable : Configurable {
 
     override fun reset() {
         textField.text = properties.adbLocation
+        verifyAdbLocation()
     }
 
     private fun adbLocationChooserDescriptor(): FileChooserDescriptor = when {
@@ -80,20 +114,36 @@ class AdbWifiConfigurable : Configurable {
         else -> FileChooserDescriptorFactory.createSingleFolderDescriptor()
     }
 
-    private companion object {
-
-        private val DIRECTORY_TEXT_COMPONENT_ACCESSOR = object : TextComponentAccessor<JTextField> {
-            override fun getText(component: JTextField) = component.text
-
-            override fun setText(component: JTextField, text: String) {
-                val file = File(text)
-                val dirName = if (file.isFile) {
-                    file.parent.orEmpty()
-                } else {
-                    text
-                }
-                component.text = dirName
-            }
+    private fun showVerifiedMessage() {
+        statusLabel.apply {
+            icon = OK_ICON
+            text = ADB_VERIFIED_MESSAGE
+            foreground = JBColor.foreground()
         }
+    }
+
+    private fun showVerificationErrorMessage() {
+        statusLabel.apply {
+            statusLabel.icon = ERROR_ICON
+            text = ADB_VERIFICATION_ERROR_MESSAGE
+            foreground = JBColor.RED
+        }
+    }
+
+    private fun verifyAdbLocation() {
+        val dir = textField.text
+        val adbFile = File("$dir/adb")
+        if (adbFile.isFile) {
+            showVerifiedMessage()
+        } else {
+            showVerificationErrorMessage()
+        }
+    }
+
+    private companion object {
+        private val OK_ICON = IconLoader.getIcon("AllIcons.General.InspectionsOK")
+        private val ERROR_ICON = IconLoader.getIcon("AllIcons.General.Error")
+        private val ADB_VERIFIED_MESSAGE = MyBundle.message("adbLocationVerifiedMessage")
+        private val ADB_VERIFICATION_ERROR_MESSAGE = MyBundle.message("adbLocationVerificationErrorMessage")
     }
 }
