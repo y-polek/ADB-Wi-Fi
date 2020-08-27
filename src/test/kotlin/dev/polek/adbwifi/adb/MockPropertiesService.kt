1ed5c3b3cae387f6f7ffbd17d31f11f691b63586
Yury Polek <yury.polek@gmail.com>
2020-08-27 22:48:38 +0300

    fix tests

--- src/test/kotlin/dev/polek/adbwifi/adb/MockPropertiesService.kt
+++ src/test/kotlin/dev/polek/adbwifi/adb/MockPropertiesService.kt
@@ -4,5 +4,6 @@ import dev.polek.adbwifi.services.PropertiesService
 
 class MockPropertiesService(
     override var isLogVisible: Boolean = false,
-    override var adbLocation: String = "/bin"
+    override var adbLocation: String = "/bin",
+    override var adbLocationListener: ((location: String, isValid: Boolean) -> Unit)? = null
 ) : PropertiesService
