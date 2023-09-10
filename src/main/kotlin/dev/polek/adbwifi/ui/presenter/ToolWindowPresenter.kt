5f2c09710702152d5cef15112e7b3f6fe6b2fe42
corey <coreycullen00@live.ie>
2023-09-10 13:50:42 +0100

    customName field

--- src/main/kotlin/dev/polek/adbwifi/ui/presenter/ToolWindowPresenter.kt
+++ src/main/kotlin/dev/polek/adbwifi/ui/presenter/ToolWindowPresenter.kt
@@ -235,7 +235,7 @@ class ToolWindowPresenter : BasePresenter<ToolWindowView>() {
                     device.serialNumber == pinnedDevice.serialNumber && device.address == pinnedDevice.address
                 } == null
             }
-            .sortedBy { it.name }
+            .sortedBy { it.customName ?: it.name }
             .map { it.toViewModel() }
             .toList()
     }
