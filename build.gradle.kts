69e76eca9440543243d4336cbd4f4cfc0c24ce48
Yurii Polek <yury.polek@gmail.com>
2024-06-01 17:18:40 +0300

    add runIde block

--- build.gradle.kts
+++ build.gradle.kts
@@ -135,4 +135,8 @@ tasks {
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
         channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
     }
+
+    runIde {
+        //ideDir.set(file("/Applications/Android Studio.app/Contents"))
+    }
 }
